16a364accf394c27fbc03ff3bf704913da372d80
Solana <shreyaslakhani25@gmail.com>
2022-11-19 10:29:01 +0000

    Update helo.js

--- helo.js
+++ helo.js
@@ -1,3 +1,33 @@
-const = 12;
-const = 10;
+const a= 12;
+const b= 10;
+ 
+const c=['gd',654,true,undefined,null];
+ // Question 1
+ 
+ 
+// if(a>b){
+//     console.log("a is greater");
+// }else{
+//     console.log("b is greater")
+// }
 
+
+//Question 2
+
+
+// for(let i=0; i<3; i++){
+//     console.log(i);
+// }
+
+
+//Question 3
+
+// try{
+//     if(a>b){
+//         console.log("a is greater");
+//     }else{
+//         throw new Error("b is greater");
+//     }
+// }catch (err){
+//     console.log(err);
+// }
